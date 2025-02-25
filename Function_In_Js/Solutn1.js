e49ae970b8cc36cfd994effcdf980f997174a2b5
Tanu Yadav <144536504+tanu1312@users.noreply.github.com>
2025-02-25 21:26:15 +0530

    Update Solutn1.js

---
+++ Function_In_Js/Solutn1.js
@@ -0,0 +1,6 @@
+// Arrow function to calculate the square of a number
+const square = (num) => num * num;
+
+// Example usage
+const number = 5;
+console.log(`The square of ${number} is ${square(number)}`);
